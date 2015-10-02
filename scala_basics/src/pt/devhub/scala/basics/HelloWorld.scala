88bcc8f5c36eb124f5662d04c17e1608bc01728a
António Ribeiro <antonio.romaneiro.ribeiro@gmail.com>
2015-10-02 20:38:17 +0100

    Added a few more Scala tests.

--- scala_basics/src/pt/devhub/scala/basics/HelloWorld.scala
+++ scala_basics/src/pt/devhub/scala/basics/HelloWorld.scala
@@ -1,11 +1,8 @@
 package pt.devhub.scala.basics
 
-/**
- * @author Aribeiro
- */
 object HelloWorld {
 
   def main(args: Array[String]) {
-    println("Hello World!");
+    println("Hello World!")
   }
 }
